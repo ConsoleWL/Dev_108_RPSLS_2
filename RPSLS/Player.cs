532e1203e39f0890abf5baed1dc222ab3f139f04
Nikita Obergan <Obergannikita@gmail.com>
2023-07-25 11:15:16 -0400

    Add class Gestures, Updated class Player

--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -10,17 +10,14 @@ namespace RPSLS
     {
         //Member Variabes (HAS A)
         public string name;
-        public List<string> gestures;
-        public string chosenGesture;
         public int score;
-
+        public Gesture chosenGesture;
+        
         //Constructor
         public Player(string name)
         {
             this.name = name;
-            gestures = new List<string> {"rock", "paper", "scissors", "lizard", "Spock" };
-            chosenGesture = "";
-            score = 0;
+            score = 0;   
         }
 
         //Member Methods (CAN DO)
