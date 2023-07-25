ff4eb4836eb1bbacebfbc090eacd1e13034f9618
Nikita Obergan <Obergannikita@gmail.com>
2023-07-25 11:26:02 -0400

    Deleted class ListOfGestures, Updated Player class

--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -12,12 +12,25 @@ namespace RPSLS
         public string name;
         public int score;
         public Gesture chosenGesture;
-        
+
+        public List<Gesture> gestures = new List<Gesture>();
+        public Gesture rock = new Gesture("rock");
+        public Gesture paper = new Gesture("paper");
+        public Gesture scissors = new Gesture("scissors");
+        public Gesture lizard = new Gesture("lizard");
+        public Gesture spock = new Gesture("spock");
+
         //Constructor
         public Player(string name)
         {
             this.name = name;
-            score = 0;   
+            score = 0;
+
+            gestures.Add(rock);
+            gestures.Add(paper);
+            gestures.Add(scissors);
+            gestures.Add(lizard);
+            gestures.Add(spock);
         }
 
         //Member Methods (CAN DO)
